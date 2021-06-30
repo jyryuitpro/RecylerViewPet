38dc7cf1bd5a21cdf3ab874732244d4c44540075
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-06-30 11:46:37 +0900

    Initial commit

---
+++ app/src/main/java/jy/ryu/itpro/recylerviewpet/MainActivity.kt
@@ -0,0 +1,11 @@
+package jy.ryu.itpro.recylerviewpet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
